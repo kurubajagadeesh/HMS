74538c4ee69c13d478abac695df9708eee1561ea
Jabir345 <mdburhanjabir@gmail.com>
2025-03-12 16:57:15 +0530

    i Have Developed  The Homepage

--- ObjectRepository/Doctor_Login_Page.cs
+++ ObjectRepository/Doctor_Login_Page.cs
@@ -1,14 +1,23 @@
-﻿using OpenQA.Selenium;
+﻿using System.Net;
+using OpenQA.Selenium;
 
 namespace Repo_Inpatient_Care.ObjectRepository
 {
     public class Doctor_Login_Page
     {
-        private IWebDriver driver;
 
         public Doctor_Login_Page(IWebDriver driver)
         {
-            this.driver = driver;
+            PageFactory.InitElement(driver, this); 
         }
+
+        [FindsBy(How = How.Name, Using = "username")]
+        private IWebElement usernameText;
+
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement passwordText;
+
+
+
     }
 }
