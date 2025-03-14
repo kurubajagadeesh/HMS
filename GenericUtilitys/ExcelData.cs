108a63592400c8238e5cde3bfbe52c16c8292524
kurubajagadeesh <kurubajagadeesh63@gmail.com>
2025-03-14 17:10:33 +0530

    Excel Utility

---
+++ GenericUtilitys/ExcelData.cs
@@ -0,0 +1,9 @@
+﻿namespace Repo_Inpatient_Care.GenericUtilitys
+{
+    public class ExcelData
+    {
+        private string username;
+
+        public string Username { get => username; set => username = value; }
+    }
+}
